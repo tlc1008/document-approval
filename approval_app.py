c80c03f0aa5ccdd0671d5c8b3cd74ddcca0d836f
tlc1008 <akira.chiba@thousand-leaf.co.jp>
2026-03-31 23:09:54 +0900

    Add files via upload

--- approval_app.py
+++ approval_app.py
@@ -54,7 +54,7 @@ try:
             
             # 2. 中段の「日付」欄 (座標付近の空白へ)
             # 総重量570の下ではなく、日付の文字の真横(y=458付近)へ
-            can1.drawString(500, 350, today_str) 
+            can1.drawString(500, 347, today_str) 
             
             # 【受領印の位置修正】
             # さらに左へ (465 -> 455)
@@ -90,7 +90,7 @@ try:
                 
                 # 3. 2ページ目「日付」欄 (座標付近)
                 can2.setFont("Helvetica", 9)
-                can2.drawString(505, 680, today_str) 
+                can2.drawString(500, 690, today_str) 
                 
                 can2.save()
                 packet2.seek(0)
