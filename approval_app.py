e6e5a15170519de35aac01980251f8d0514cf818
tlc1008 <akira.chiba@thousand-leaf.co.jp>
2026-03-31 23:37:19 +0900

    Add files via upload

--- approval_app.py
+++ approval_app.py
@@ -52,8 +52,8 @@ try:
             
             # 【A. 3カ所の日付入力】文字サイズ9
             can1.setFont("Helvetica", 9)
-            can1.drawString(500, 780, today_str) # 右上の「日付」
-            can1.drawString(500, 340, today_str) # 中段の「日付」
+            can1.drawString(500, 785, today_str) # 右上の「日付」
+            can1.drawString(500, 335, today_str) # 中段の「日付」
             
             # 【B. 印影のデザインと位置】
             # 千葉様が調整された「455, 168」付近に設定しています
@@ -102,7 +102,7 @@ try:
                 
                 # 2ページ目の日付
                 can2.setFont("Helvetica", 9)
-                can2.drawString(500, 705, today_str) 
+                can2.drawString(500, 735, today_str) 
                 
                 can2.save()
                 packet2.seek(0)
